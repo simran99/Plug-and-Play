772e45bd873894ec45a6c8ca140a570c7c140c03
Parth Singh <parthsingh287@gmail.com>
2019-03-25 03:33:47 +0530

    Adding model and database

--- plug_and_play/plug_and_play/settings.py
+++ plug_and_play/plug_and_play/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'connect.apps.ConnectConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,10 +76,13 @@ WSGI_APPLICATION = 'plug_and_play.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'plug_and_play',
+        'USER': 'root',
+        'PASSWORD': '*******',
     }
 }
+
 
 
 # Password validation
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
